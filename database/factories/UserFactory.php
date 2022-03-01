2252121a3f1cae37fcdaf889480826cc8a3f30d1
unknown <ahmad.elhag28@gmail.com>
2022-03-01 15:01:33 +0200

    add fake account

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$2wTO0Y4/txlZVfcGcXbffOYhao7Vd9Dqo6RNAXiIdqrTa22a2Vsyq', // password
         'remember_token' => Str::random(10),
     ];
 });
