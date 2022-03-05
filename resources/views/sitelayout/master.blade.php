b6ed1d10571cacc031c8f05476675387d39a32b1
unknown <ahmad.elhag28@gmail.com>
2022-03-05 18:01:48 +0200

    ---

--- resources/views/sitelayout/master.blade.php
+++ resources/views/sitelayout/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -10,6 +11,7 @@
     <link rel="icon" type="image/x-icon" href="{{ URL::asset('site/assets/favicon.ico') }}" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    {{-- <link href="{{ URL::asset('/css/fonts/bootstrap-icons.css') }}" rel="stylesheet" /> --}}
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="{{ URL::asset('site/css/styles.css') }}" rel="stylesheet" />
 </head>
@@ -23,6 +25,8 @@
     @include('site_include.footer')
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="{{ URL::asset('/js/bootstrap.bundle.min.js') }}"></script> --}}
+    
     <!-- Core theme JS-->
     <script src="{{ URL::asset('site/js/scripts.js') }}"></script>
 </body>
